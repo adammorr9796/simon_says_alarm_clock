74bf48602f77af5c3501353f45bde7eaae9c1a35
Adam Morrison <adammorr9796@gmail.com>
2022-03-14 23:14:44 -0500

    Rework button thread and button interrupt handling

--- main.py
+++ main.py
@@ -8,39 +8,27 @@ import RPi.GPIO as GPIO
 exit_evt = threading.Event()
 exit_evt.clear()
 
-bttn_0_evt = threading.Event()
-bttn_1_evt = threading.Event()
-bttn_2_evt = threading.Event()
-bttn_3_evt = threading.Event()
-
-bttn_0_evt.clear()
-bttn_1_evt.clear()
-bttn_2_evt.clear()
-bttn_3_evt.clear()
+bttn_evt = threading.Event()
+bttn_evt.clear()
 
 clk_face = display.Display(0x70, 1)
 clk_face.setup()
 clk_face.write_colon(True)
 
 cur_time = datetime.now()
-bttn_q = queue.Queue(10)
+bttn_q = queue.Queue()
 
 BTTN_0 = 0
 BTTN_1 = 1
 BTTN_2 = 2
 BTTN_3 = 3
 
-def bttn_callbk_0(channel):
-    bttn_0_evt.set()
+MODE = 0       # 0 == CLK_MODE, 1 == ALRM_MODE, 2 == SIMON_MODE
 
-def bttn_callbk_1(channel):
-    bttn_1_evt.set()
-
-def bttn_callbk_2(channel):
-    bttn_2_evt.set()
-
-def bttn_callbk_3(channel):
-    bttn_3_evt.set()
+def bttn_callbk(channel):
+    global cur_bttn
+    cur_bttn = channel
+    bttn_evt.set()
 
 def clk_main():
     print("clk_main started")
@@ -66,6 +54,7 @@ def clk_main():
         clk_face.write_digit(3, int(minute % 10), pm)
 
 def bttn_main():
+    global MODE
     print("bttn_main started")
     GPIO.setmode(GPIO.BCM)
 
@@ -74,27 +63,28 @@ def bttn_main():
     GPIO.setup(27, GPIO.IN, pull_up_down=GPIO.PUD_UP)
     GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-    GPIO.add_event_detect(10, GPIO.RISING, callback=bttn_callbk_0, bouncetime=100)
-    GPIO.add_event_detect(22, GPIO.RISING, callback=bttn_callbk_1, bouncetime=100)
-    GPIO.add_event_detect(27, GPIO.RISING, callback=bttn_callbk_2, bouncetime=100)
-    GPIO.add_event_detect(17, GPIO.RISING, callback=bttn_callbk_3, bouncetime=100)
+    GPIO.add_event_detect(10, GPIO.FALLING, callback=bttn_callbk, bouncetime=100)
+    GPIO.add_event_detect(22, GPIO.FALLING, callback=bttn_callbk, bouncetime=100)
+    GPIO.add_event_detect(27, GPIO.FALLING, callback=bttn_callbk, bouncetime=100)
+    GPIO.add_event_detect(17, GPIO.FALLING, callback=bttn_callbk, bouncetime=100)
 
     while(not exit_evt.is_set()):
-        if (bttn_0_evt.is_set()):
-            bttn_q.put(BTTN_0)
-            bttn_0_evt.clear()
-
-        if (bttn_1_evt.is_set()):
-            bttn_q.put(BTTN_1)
-            bttn_1_evt.clear()
-
-        if (bttn_2_evt.is_set()):
-            bttn_q.put(BTTN_2)
-            bttn_2_evt.clear()
-
-        if (bttn_3_evt.is_set()):
-            bttn_q.put(BTTN_3)
-            bttn_3_evt.clear()
+        if (bttn_evt.is_set() and (cur_bttn == 10 or cur_bttn == 22 or cur_bttn == 17)):
+            bttn_q.put(cur_bttn)
+            bttn_evt.clear()
+        elif (bttn_evt.is_set() and cur_bttn == 27):
+            if (MODE == 0):
+                #if in CLK_MODE
+                #set_blink(0)
+                MODE = 1 #set to ALRM_MODE
+            elif (MODE == 1):
+                #if in ALRM_MODE
+                #set_blink(1)
+                MODE = 0
+            elif (MODE == 2):
+                #if in SIMON_MODE
+                bttn_q.put(27)
+            bttn_evt.clear()
 
 clk_thread = threading.Thread(target=clk_main)
 bttn_thread = threading.Thread(target=bttn_main)
